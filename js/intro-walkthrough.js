ea792a7c87b5e8c4e781ce0d8b99e24755db5363
Luisan <luisangelsc@gmail.com>
2019-08-30 12:39:31 -0700

    remove bullets, add progress bar since I have a fixed sidebar, if users want to jump around the tour using the bullets they might break the tour (if they're still on the welcome page and they skip to the sidebar part of the tour it won't scroll down, but instead highlight the hamburger button where it currently is, hidden behind the welcome screen

--- js/intro-walkthrough.js
+++ js/intro-walkthrough.js
@@ -5,9 +5,6 @@ function startIntro(){
         {
           intro: "Welcome :) This is Luisan's assistant, I'll be showing you around. Click 'next' whenever you're ready to move on, 'back' if you want to go back, and 'skip' or outside this window to exit this walkthrough."
         },
-        // {
-        //   intro: "You <b>don't need</b> to define element to focus, this is a floating tooltip."
-        // },
         {
           element: '#step1',
           intro: "This button will take you Luisan's projects. Click next to have me take you to them!"
@@ -25,48 +22,46 @@ function startIntro(){
         {
           element: '#step5',
           intro: 'Open the menu to see more details about each button',
-          position: "right"
+          position: "bottom"
         },
         {
           element: '#step6',
-          intro: 'Here is where you can reach Luisan'
+          intro: 'Here is where you can reach Luisan',
+          position: "right"
         },
         {
           element: '#step7',
-          intro: "That's it! If you need me again, just click on me again. I'll be in my corner!"
+          intro: "That's it! If you need me, just click on me again. I'll be in my corner!"
         }
       ]
-    });
+    })
+    intro.setOption('showBullets', false);
+    intro.setOption('showProgress', true)
+
     intro.onafterchange(function(targetElement) {
-        console.log(this._currentStep);
         if(this._currentStep === 1){
-            console.log("step 1");
             largeHeader = document.getElementById("large-header");
-            console.log(largeHeader);
             largeHeader.classList.remove("introjs-fixParent")
         }
         if(this._currentStep === 4){
             overlay = document.getElementsByClassName("introjs-overlay")[0];
             helperLayer = document.getElementsByClassName("introjs-helperLayer")[0];
             sidebar = document.getElementById("sidebar");
+
             overlay.style.zIndex = "0";
             helperLayer.style.zIndex = "0";
             sidebar.classList.remove("introjs-fixParent")
-            console.log(overlay);
-            console.log(helperLayer);
-            console.log(sidebar);
-            console.log(window.pageYOffset);
-            window.scroll(0,1144);
-
         }
         if(this._currentStep === 5){
             overlay = document.getElementsByClassName("introjs-overlay")[0];
             helperLayer = document.getElementsByClassName("introjs-helperLayer")[0];
             sidebar = document.getElementById("sidebar");
+
             overlay.style.zIndex = "0";
             helperLayer.style.zIndex = "0";
             sidebar.classList.remove("introjs-fixParent")
         }
     });
+
     intro.start();
 }
